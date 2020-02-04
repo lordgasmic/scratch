0ec11a290cdc9d4b50cde329619fedbb64e32d5a
lordgasmic <wellman.nicholas@gmail.com>
2020-02-04 16:30:48 -0500

    vernam

---
+++ VernamResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vernam
+{
+    public class VernamResponse
+    {
+        public String OTP { get; set; }
+        public String EncodedMessage { get; set; }
+    }
+}
